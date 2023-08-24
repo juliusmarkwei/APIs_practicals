1facffcb7807046d80429884ffb1a8f103ced9d4
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-08-24 16:09:52 +0100

    doc1 args

--- request.py
+++ request.py
@@ -1,6 +1,7 @@
 import requests
 
-url = 'https://api.openweathermap.org/data/2.5/weather?q=London&APPID={f3e1216cc7e05d7e1db59db5a7ca97ec}'
+key = 'b1128cc40237ba85a36baec505b7bfc5'
+url = f'https://api.openweathermap.org/data/2.5/weather?q=London&APPID={key}'
 r = requests.get(url)
 
-print()
+print(r.json()['main']['humidity'])
